21e62d0ed15ebc1d08c0f731977aac66d6999364
manit <manit.instep248@gmail.com>
2024-07-31 17:30:15 +0530

    Technical Tickets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Routers } from './routes';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
-  return <Routers />
+  return (
+    <>
+     <Helmet>
+        <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+
+        <meta http-equiv="Content-Security-Policy" content={`connect-src 'self' ${process.env.REACT_APP_BASE_URL};`} />
+        <meta name="server" content="Custom Server" />
+        <meta http-equiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains" />
+           </Helmet>
+      <Routers />
+    </>
+ 
+)
 }
 
 export default App
