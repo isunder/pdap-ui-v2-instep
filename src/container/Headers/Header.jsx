fb26c11b73ff231d0bbb3dd17e4ce2bdf0b505b1
manit <manit.instep248@gmail.com>
2024-08-26 12:47:04 +0530

    New Changes

--- src/container/Headers/Header.jsx
+++ src/container/Headers/Header.jsx
@@ -319,7 +319,7 @@ export const Header = ({ sessionObject }) => {
                 </Grid>
 
                 {
-                  tabs && !(tabs?.patient_dashboard_recapture_percentage?.active && tabs?.patient_dashboard_suspect_percentage?.active) ?
+                  tabs && (tabs?.patient_dashboard_recapture_percentage?.active || tabs?.patient_dashboard_suspect_percentage?.active) ?
                     <Grid className="suspect_recapture_header" item md={7} sm={6}>
                       <Box
                         sx={{
@@ -397,6 +397,7 @@ export const Header = ({ sessionObject }) => {
                               </StyleText>
                             </Box>
                           )}
+
                         {tabs &&
                           tabs["patient_dashboard_suspect_percentage"] && tabs["patient_dashboard_suspect_percentage"].active
 
