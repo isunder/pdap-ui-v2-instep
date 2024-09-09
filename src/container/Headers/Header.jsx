f197839a5d384d00b958f3e2708f2e083150e87e
manit <manit.instep248@gmail.com>
2024-09-09 15:10:46 +0530

    Fix doctor name issue

--- src/container/Headers/Header.jsx
+++ src/container/Headers/Header.jsx
@@ -319,7 +319,7 @@ export const Header = ({ sessionObject }) => {
                 </Grid>
 
                 {
-                  tabs && (tabs?.patient_dashboard_recapture_percentage?.active || tabs?.patient_dashboard_suspect_percentage?.active) ?
+                  tabs && (tabs?.patient_dashboard_recapture_percentage?.active || tabs?.patient_dashboard_suspect_percentage?.active) && doctorDetail?.doctor_name ?
                     <Grid className="suspect_recapture_header" item md={7} sm={6}>
                       <Box
                         sx={{
