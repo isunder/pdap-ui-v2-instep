0abf8f32348e1a18da6c2e5f9f4d124d3de3fe7c
InStep Technologies <code@insteptechnologies.com>
2024-10-08 16:31:38 +0530

    New Fixessss

--- src/components/SubmitModal/SubmitModal.jsx
+++ src/components/SubmitModal/SubmitModal.jsx
@@ -411,7 +411,8 @@ const SubmitModal = ({
                                         title={
                                           Object.keys(item) +
                                           " : " +
-                                          item[Object.keys(item)].value
+                                          ([Object.keys(item)].code) ? (item[Object.keys(item)].value) : null
+
                                         }
                                       >
                                         <Typography>
